e448f751b4e948da4ae60e0b05e35ea70d2d5eb0
Rafa Khadafi <rafakhadafi29@gmail.com>
2025-07-09 14:09:06 +0700

    generate

--- localhost.session.sql
+++ localhost.session.sql
@@ -11,3 +11,15 @@ create table sample (
      nama varchar(100) not null,
      constraint primary_key_id primary key (id)
      )engine = innodb;
+
+
+-- 2 membaut tabel customer (setelah buat model di prisma)
+create table customer (
+     id varchar(100) not null,
+     nama VARCHAR(100) not null,
+     email varchar(100) not null,
+     phone varchar(100) not null,
+     constraint primaryKeyId primary key (id),
+     constraint emailUnique unique (email),
+     constraint phoneUnique unique (phone)
+)engine = innodb;
