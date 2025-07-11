e2a486e56ac7ccc95f153d07dd4a061efc5c6d82
Rafa Khadafi <rafakhadafi29@gmail.com>
2025-07-11 10:31:01 +0700

    delete-many

--- test/crud/7.delete.test.ts
+++ test/crud/7.delete.test.ts
@@ -4,20 +4,35 @@ describe("testing delete data", () => {
   // disini kita tambahkan dulu data yang salah
   it("crete record salah", async () => {
     // jadi ini dulu baru yg delete
-    const dataImpact: Rafa.TypeCustomer = await prismaClient.customer.create({
-      data: {
+    const dataImpact = await prismaClient.customer.createMany({
+      data: [
+        {
         id: "007",
         nama: "salah",
         email: "salah12@gmail.com",
-        phone: "99864444",
+        phone: "998644344",
       },
+        {
+        id: "008",
+        nama: "salah2",
+        email: "salah1226@gmail.com",
+        phone: "9986443344244",
+      },
+        {
+        id: "003",
+        nama: "salah2",
+        email: "salah122@gmail.com",
+        phone: "9986434244",
+      },
+
+    ],
     });
 
     console.log(dataImpact)
     // berhasil, dan return dta yg di create / insert
   });
 
-  it.only("testing delete 1 data", async () => {
+  it("testing delete 1 data", async () => {
     // jadi where ini wajib untuk ada dan unique atau
     // field yg bukan unique tapi yakin unique
     const dataImpact = await prismaClient.customer.delete({
@@ -35,4 +50,20 @@ describe("testing delete data", () => {
     console.log(dataImpact)
     // return data yang di delete
   });
+
+  it.only('delete data yang bnyak sekaligus', async () => {
+    // jadi kita akan delete data nama yang 
+    // memiliki kata salah
+
+    const dataImpact = await prismaClient.customer.deleteMany({
+      where:{
+        nama:{
+          contains:
+          "salah",
+        }
+      }
+    })
+
+    console.log(dataImpact) // { count: 3 }
+  })
 });
